80a6ddea020fb504483b49b1940b20c2e8fb7c94
otaviopassarelli <otaviopassarelli@outlook.com>
2016-05-13 10:21:38 -0300

    Código-fonte do projeto
    Código modularizado em C

--- Código-fonte do projeto.cpp
+++ Código-fonte do projeto.cpp
@@ -1,37 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+/* Pega a quantidade de números */
+int quantidade(int N)
 {
-	int N=0;
-	int *qtd_num;
-	int valor, i, vlr, par=0, impar=0;
-	
 	printf("Insira a quantidade de numeros: ");
 	scanf("%d", &N);
-	     
-	qtd_num = (int *) calloc(N, sizeof(int));
-		
-	printf("Insira os numeros desejados (somente inteiros):\n");
-	for(i=0; i < N; i++)
-	{
-		scanf("%d", &vlr);
-		qtd_num[i] = vlr;	
-	}
+	return N;
+}
+
+/* Insere os números desejados no vetor */
+int insere_no_vetor(int N, int *qtd_num)
+{
+	int i;
 	
-	for(i=0; i < N; i++)
+	printf("Insira os numeros desejados:\n");
+	for(i=0; i < N; i++) //percorre o vetor
 	{
-		if(qtd_num[i]%2 == 0)
+		scanf("%d", &qtd_num[i]); //Insere ordenadamente cada número digitado em uma posição do vetor
+	}
+	return *qtd_num;
+}
+
+/* Conta números pares e ímpares */
+void conta_par_impar(int *qtd_num, int N)
+{	
+	int i, par=0, impar=0;
+	
+	for(i=0; i < N; i++) //percorre o vetor
+	{
+		if(qtd_num[i]%2 == 0) //Se o resto da divisão por 2 for 0, "par" é incrementado
 			par++;
 		
 		else
-			impar++;
+			impar++; //Se não for 0, "impar" é incrementado
 	}
-	
-	free(qtd_num);
 	printf("\nQuantidade de numeros impares: %d\n", impar);
 	printf("Quantidade de numeros pares: %d\n\n", par);
+}
+
+int main()
+{
+	int N, *qtd_num;
 	
+	N=quantidade(N);
+	qtd_num = (int *) calloc(N, sizeof(int)); //Aloca memória necessária para o vetor
+	insere_no_vetor(N, qtd_num);	
+	conta_par_impar(qtd_num, N);	
+	free(qtd_num); //Libera a memória alocada
 	return 0;
-	
 }
